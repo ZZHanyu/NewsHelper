5eed28ba1501fee14281654da78071fce9bd32b4
HanFish <2538285586@qq.com>
2024-04-07 23:38:55 +0800

    data_handler_Done

--- classifier.py
+++ classifier.py
@@ -11,51 +11,53 @@ from utils import preprocess
 # from utils import custom_dataloader
 
 
+class main:
+    def __init__(self, main_args) -> None:
+        self.args = main_args
+        self._chunks = None
 
 
-
-
-'''
-    Part 1: Load Dataset
-'''
-def dataloader(args):
-    try:
-        chunks = pd.read_csv(args.dataset_path, chunksize=args.chunk_size)
-        for chunk in chunks:
-            # print(chunk)      
-            for index, row in chunk.iterrows():
-                print(f"{index}\t {row} \n")          
-    except Exception as e:
-        print(f"Data Loading Failed! {e}")
+    def _dataloader(self):
+        '''
+            ---------- Part 1: Dataset Loader ----------
+            Objective:
+                - using batchs(chunks) to load csv line by line
+            Params:
+                - args: chunk size defined
+        '''
+        try:
+            self._chunks = pd.read_csv(self.args.dataset_path, chunksize=self.args.chunk_size)
+        except Exception as e:
+            print(f"Data Loading Failed! {e}")
     
     
 
+    def _data_preprocess(self):
+        '''
+            ---------- Part 2: Text Preprocess ----------
+            Objective:
+                - clean the symbols(,.%) and space 
+                - lowercase the letters
+            Params:
+
+        '''
+        data_handler = preprocess.charactors_hander(chunks=self._chunks)
+        data_handler.run()
+
+    def forward(self):
+        self._dataloader()
+        self._data_preprocess()
 
 
 
-'''
-    Part 2: Text Preprocess
-    - clean the symbols(,.%) and space 
-    - lowercase the letters
-    - 
-    
-'''
 
+# Start from here
+parser = argparse.ArgumentParser(description="Parameters for Classifier")
+parser.add_argument("--dataset_path", type=str, default="utils/WELFake_Dataset.csv", help="the path of your dataset")
+parser.add_argument("--chunk_size", type=int, default=10000, help="control how many lines read once")
+args = parser.parse_args()
 
-
-
-
-
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Parameters for Classifier")
-    parser.add_argument("--dataset_path", type=str, default="utils/WELFake_Dataset.csv", help="the path of your dataset")
-    parser.add_argument("--chunk_size", type=int, default=10000, help="control how many lines read once")
-
-    args = parser.parse_args()
-
-    #mydataset = custom_dataloader.CustomDataset()
-    dataloader(args=args)
+main_progress = main(args)
+main_progress.forward()
 
 
--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,34 +1,84 @@
 from transformers import PreTrainedTokenizerFast
+import pandas
+from tqdm import tqdm
+import time 
+import re
+import json
 
 
 class charactors_hander:
-    def __init__(self, strs:str) -> None:
-        self._raw_str = strs
+    def __init__(self, chunks: pandas.io.parsers.readers.TextFileReader) -> None:
+        self._meta_data = chunks
         self.tokenizer = PreTrainedTokenizerFast.from_pretrained('bert-base-uncased')
-
+        # self.tokenizer.pad_token = self.tokenizer.eos_token
+        self.tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+        self._json_result = {}
         
-    def _empty_judge(self) -> bool:
-        if len(self._raw_str) <= 0:
-            return False
-        else:
-            return True
+    # def _remove_empty(self, chunk, *args):
+    #     for arg in args:
+    #         chunk = chunk[len(chunk[arg]) != 0]
 
-    def _lower_charactor(self)->str:
-        return self._raw_str.lower()
-    
-    def _remove_spaces(self)->str:
-        for idx in range(len(self._raw_str)):
-            if self._raw_str[idx] == " ":
-                del self._raw_str[idx] 
-        return self._raw_str
 
     def _split_single_word(self) -> list:
         return self._raw_str.split()
     
     def display_elements(self):
         return self._raw_str
-    
-    #def _word_tokenizer(self):
+
+    def run(self):
+        # try:
+        for chunk in tqdm(self._meta_data, desc="Layer1: Data Preprocess"):
+            # Step1: check whether empty
+                # if a blank dectected, then delete this line
+            # self._remove_empty(chunk, 'title', 'text', 'label')
+
+            # STEP 1: Remove empty line (Remove dirty data)
+            chunk = chunk[(chunk['title'].notnull()) & (chunk['title'].dtypes == object)]
+            
+            chunk = chunk[(chunk['text'].apply(lambda x: True if (x != None) & (type(x) == str) else False))]
+            chunk = chunk[(chunk['label'].notnull()) & (chunk['label'].dtypes == int)]
+
+            for index, row in chunk.iterrows():
+                #print(f"index = {index}\n context = {row}, \ntitle = {row['title']}\n type = {type(row['title'])},\n length={len(row['title'])} \n")
+                            
+                # STEP 2: Lower all charactors in string
+                row['title'] = row['title'].lower()
+                row['text'] = row['text'].lower()
+
+                print(f"{len(row['text'])}\n")
+                
+                # split
+                row['title'] = row['title'].split()
+                row['text'] = row['text'].split()
+
+                # tokenizer
+                print(f"{row['text']} {len(row['text'])} {type(row['text'])}\n")
+                encoder_title = self.tokenizer(row['title'], padding=True, return_tensors='pt')
+                encoder_text = self.tokenizer(row['text'], padding=True,  return_tensors='pt')
+                #print(f"token result = title = {encoder_title}\n text = {encoder_text}\n\n")
+                #time.sleep(5)
+                self._json_result[index] = (encoder_title, encoder_text, row['label'])
+        print(self._json_result)
+                #print(f"**After modify : \t title = {row['title']}, context = {row['text']}, label = {row['label']} \n\n")
+
+        # except Exception as e:
+        #     print(f"Data Preprocessing Failed! error is {e}")
+
+
+        
+        # if self._empty_judge() == False:
+        #     assert 1==0, print("** ERROR! there are blank line exist")
+        
+        
+
+
+        # split into single words set
+
+
+
+        #encoded_inputs = self.tokenizer(self._raw_str, padding=True, return_tensors='pt')
+        #print(encoded_inputs)
+
 
     
 
